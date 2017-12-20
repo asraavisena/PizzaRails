c22518b31034110327659c899dd194d694618d1a
asraavisena <asra.avisena@yahoo.com>
2017-12-20 17:08:52 +0100

    already create, migrate, seed db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171122163019) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cumstomers", force: :cascade do |t|
     t.integer "customerID"
     t.string "name"
